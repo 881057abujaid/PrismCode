67b1bd3aafa133e8bf1e9779a2ab9510cfe5370d
Abujaid Raja <abujaidraja.com>
2026-07-11 08:28:31 +0530

    feat: integrate form validation with React Hook Form and Zod

---
+++ frontend/src/validations/auth.validation.js
@@ -0,0 +1,15 @@
+import { email, z } from "zod";
+
+export const loginSchema = z.object({
+    email: z.email("Please enter a valid email address."),
+
+    password: z.string().min(8, "Password must be at least 8 characters."),
+});
+
+export const registerSchema = z.object({
+    name: z.string().trim().min(3, "Name must be at least 3 characters.").max(50, "Name cannot exceed 50 characters."),
+
+    email: z.email("Please enter a valid email address."),
+
+    password: z.string().min(8, "Password must be at least 8 characters."),
+});
